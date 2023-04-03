6c5c10b8b62784c67c35c5a5deb73c6ba756ee9e
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-04-03 11:37:03 -0600

    version 1

--- 04-Clases.js
+++ 04-Clases.js
@@ -17,3 +17,38 @@ const jorge = new Student('Jorge', 21, ['HTML', 'CSS', 'JS']);//Creacion del obj
 jorge.showInformation();//Informacion original
 jorge.addCertify('PHP');//Agregamos un nuevo curso
 jorge.showInformation();//Mostramos la nueva Informacion
+
+
+
+//Otro ejemplo de clase estudiante
+class Student {
+    constructor({//Le mandaremos un objeto que contenga estos atributos
+        name,
+        email,
+        username,
+        twitter = undefined, //valores por defecto ya que no es necesario que lo tengan
+        instagram = undefined, //valores por defecto ya que no es necesario que lo tengan
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = []
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+};
+const jorge2 = new Student({
+    name: 'Jorge',
+    email: 'jorgedeltoro111@outlook.es',
+    username: 'neverland',
+    facebook: 'Jorge Enrique',
+    learningPaths: ['Programacion web', 'React'],
+});
+console.log(jorge);
